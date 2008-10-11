7aa6e2fda55bc652f881151ba655ccb94bf13913
cthulhu32 <cthulhu32@5fe1ab44-955a-11dd-a953-6f7e5726be69>
2008-10-11 02:12:28 +0000

    Slowly rewriting the Snes9X GX gui and file handling to match FBA GX. Will eventually try to make a new menu layout, but for now Snes9x GX has an extremely clean interface and works great! Thanks Tantric and other guys.

---
+++ code/ngc/dvd.h
@@ -0,0 +1,28 @@
+/****************************************************************************
+ * Final Burn Alpha GX 0.1 Nintendo Wii/Gamecube Port
+ *
+ * softdev July 2006
+ * svpe & crunchy2 June 2007
+ * Tantric September 2008
+ * Cthulhu32 October 2008
+ *
+ * dvd.h
+ *
+ * DVD I/O functions
+ ***************************************************************************/
+
+#ifndef _NGCDVD_H_
+#define _NGCDVD_H_
+
+int getpvd ();
+int ParseDVDdirectory ();
+int LoadDVDFile (unsigned char *buffer, int length);
+bool TestDVD();
+int dvd_read (void *dst, unsigned int len, u64 offset);
+bool SwitchDVDFolder(char dir[]);
+void SetDVDDriveType();
+#ifdef HW_DOL
+void dvd_motor_off ();
+#endif
+
+#endif
---
+++ code/ngc/menu.h
@@ -0,0 +1,22 @@
+/****************************************************************************
+ *  Final Burn Alpha GX 0.1 Nintendo Wii/Gamecube Port
+ *
+ * softdev July 2006
+ * crunchy2 May-June 2007
+ * Michniewski 2008
+ * Tantric August 2008
+ * ----- Snes9xgx ------
+ * Cthulhu32 October 2008
+ *
+ * menu.h
+ *
+ * Menu flow routines - handles all menu logic
+ ***************************************************************************/
+
+#ifndef _NGCMENU_
+
+#define _NGCMENU_
+
+void MainMenu (int selectedMenu);
+
+#endif
