0d8af2af31be05cba31c1d5f11c3bda905da0ef1
cthulhu32 <cthulhu32@5fe1ab44-955a-11dd-a953-6f7e5726be69>
2008-10-27 05:21:26 +0000

    Rom checking in the works, at least it looks for the zip file and spits out the contents of what should be in there (a bunch of blank files is weird, but I'll dig deeper for info on that.)

--- trunk/code/ngc/roms.cpp
+++ trunk/code/ngc/roms.cpp
@@ -1,6 +1,22 @@
 #include "roms.h"
 #include "burn.h"
 
+#include "fbagx.h"
+#include "menudraw.h"
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BZIP_MAX 32
+
+static char* szBzipName[BZIP_MAX] = { NULL, };					// Zip files to search through
+
+struct RomFind { int nState; int nZip; int nPos; };				// State is non-zero if found. 1 = found totally okay.
+static int nRomCount = 0; static int nTotalSize = 0;
+static int nListCount = 0;
+static int nCurrentZip = -1;									// Zip which is currently open
+
 int AnalyzeRoms()
 {
 	// Go through the list of Zips
@@ -35,6 +51,46 @@ int debugFillMenu(char ** menuArray){
 		BurnDrvGetZipName(&menuArray[z],z);	
 	}
 	return 0;
-
 }
 
+int checkRom(char * filename)
+{
+	for (unsigned int z = 0; z < nBurnDrvCount; z++) {
+		nBurnDrvSelect = z;
+		char * szName = NULL;
+		BurnDrvGetZipName(&szName, 0); // this will return 1 if not found
+		if (strncmp(szName,filename,strlen(szName))==0){ // not a memcpy :D
+			char msg[256];
+			sprintf(msg, "Found driver for %s, number %i/%i", szName, z, nBurnDrvCount);			
+			WaitPrompt(msg);
+			
+			// Count the number of roms needed
+			for (nRomCount = 0; ; nRomCount++) {
+				if (BurnDrvGetRomInfo(NULL, nRomCount)) {
+					break;
+				}
+			}
+			if (nRomCount <= 0) {
+				WaitPrompt((char *)"No roms found for nRomCount.");	
+				return 1;
+			}
+
+			for (int i = 0; i < nRomCount; i++) {
+				struct BurnRomInfo ri;
+				memset(&ri, 0, sizeof(ri));
+				if(BurnDrvGetRomInfo(&ri, i)) break;								// Get info about the rom
+				char* romName = "Unknown";
+				BurnDrvGetRomName(&romName,i, 0);
+				if ( strlen(romName) == 0) continue;				
+				char msg[256];
+				sprintf(msg, "Rom name: %s %i/%i", romName, i, nRomCount);
+				WaitPrompt(msg);			
+			}
+			return 1;		
+		}	
+	}
+	char msg[256];
+	sprintf(msg, "Could not find a driver for %s", filename);
+	WaitPrompt(msg);
+	return 0;
+}
