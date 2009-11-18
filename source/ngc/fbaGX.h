7962a554b1745651a715e12cdaf012421b674461
cthulhu32 <cthulhu32@5fe1ab44-955a-11dd-a953-6f7e5726be69>
2009-11-18 23:42:50 +0000

    Oops, here's interfacegx (will rename once I know its working and i can remove the orig.) Also changed the input a little bit so our fbaGX is getting closer to the original.

---
+++ source/ngc/fbaGX.h
@@ -0,0 +1,44 @@
+#ifndef _DEMO_H_
+#define _DEMO_H_
+
+#include "FreeTypeGX.h"
+
+enum {
+	METHOD_AUTO,
+	METHOD_SD,
+	METHOD_USB,
+	METHOD_DVD,
+	METHOD_SMB,
+	METHOD_MC_SLOTA,
+	METHOD_MC_SLOTB,
+	METHOD_SD_SLOTA,
+	METHOD_SD_SLOTB
+};
+
+enum
+{
+	CTRL_PAD,
+	CTRL_MOUSE,
+	CTRL_SCOPE,
+	CTRL_JUST,
+	CTRL_PAD2,
+	CTRL_PAD4,
+	CTRL_LENGTH
+};
+
+struct SSettings {
+    int		AutoLoad;
+    int		AutoSave;
+    int		LoadMethod;
+	int		SaveMethod;
+	char	Folder1[256]; // Path to files
+	char	Folder2[256]; // Path to files
+	char	Folder3[256]; // Path to files
+};
+extern struct SSettings Settings;
+
+void ExitApp();
+extern int ExitRequested;
+extern FreeTypeGX *fontSystem[];
+
+#endif
--- source/ngc/input.h
+++ source/ngc/input.h
@@ -1,9 +1,14 @@
 /****************************************************************************
- * libwiigui Template
- * Tantric 2009
+ * Snes9x 1.51 Nintendo Wii/Gamecube Port
+ *
+ * softdev July 2006
+ * crunchy2 May-June 2007
+ * Michniewski 2008
+ * Tantric 2008-2009
  *
  * input.h
- * Wii/GameCube controller management
+ *
+ * Wii/Gamecube controller management
  ***************************************************************************/
 
 #ifndef _INPUT_H_
@@ -14,12 +19,19 @@
 
 #define PI 				3.14159265f
 #define PADCAL			50
+#define MAXJP 			12 // # of mappable controller buttons
 
+extern u32 btnmap[4][4][12];
 extern int rumbleRequest[4];
 
-void SetupPads();
-void UpdatePads();
+void ResetControls(int cc = -1, int wc = -1);
 void ShutoffRumble();
 void DoRumble(int i);
+void NGCReportButtons ();
+void SetControllers ();
+void SetDefaultButtonMap ();
+bool MenuRequested();
+void SetupPads();
+void UpdatePads();
 
 #endif
