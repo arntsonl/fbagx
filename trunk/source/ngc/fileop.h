b36b47f4bcccb27b039cb9c93b40554cc190cbb9
Cthulhu32 <Cthulhu32@5fe1ab44-955a-11dd-a953-6f7e5726be69>
2009-11-22 07:07:10 +0000

    Updated a few things, sort of loads roms (doesn't actually extract the zips maybe)

---
+++ trunk/source/ngc/fileop.h
@@ -0,0 +1,6 @@
+#ifndef __FILEOP_H__
+#define __FILEOP_H__
+
+int LoadBurnerRom(const char * fileName, const char * fileDir);
+
+#endif
---
+++ trunk/source/ngc/media.cpp
@@ -0,0 +1,95 @@
+// Media module
+#include "burner.h"
+
+int MediaInit()
+{
+/*
+	if (ScrnInit()) {					// Init the Scrn Window
+		FBAPopupAddText(PUF_TEXT_DEFAULT, MAKEINTRESOURCE(IDS_ERR_UI_WINDOW));
+		FBAPopupDisplay(PUF_TYPE_ERROR);
+		return 1;
+	}
+*/
+	// Screen Size will go here via
+	/*
+		if (bDrvOkay) {
+		if ((BurnDrvGetFlags() & BDF_ORIENTATION_VERTICAL) && (nVidRotationAdjust & 1)) {
+			BurnDrvGetVisibleSize(&nBmapHeight, &nBmapWidth);
+			BurnDrvGetAspect(&nGameAspectY, &nGameAspectX);
+		} else {
+			BurnDrvGetVisibleSize(&nBmapWidth, &nBmapHeight);
+			BurnDrvGetAspect(&nGameAspectX, &nGameAspectY);
+		}
+
+		if (nBmapWidth <= 0 || nBmapHeight <= 0) {
+			return 1;
+		}
+	}
+	*/
+
+	if (!bInputOkay) {	
+		InputInit();					// Init Input
+	}
+
+//	nAppVirtualFps = nBurnFPS;
+
+	if (!bAudOkay) {
+		AudSoundInit();					// Init Sound (not critical if it fails)
+	}
+
+	nBurnSoundRate = 0;					// Assume no sound
+	pBurnSoundOut = NULL;
+	if (bAudOkay) {
+		nBurnSoundRate = nAudSampleRate[nAudSelect];
+		nBurnSoundLen = nAudSegLen;
+	}
+
+	if (!bVidOkay) {
+
+		// Reinit the video plugin
+		VidInit();
+		if (!bVidOkay && nVidFullscreen) {
+
+			nVidFullscreen = 0;
+
+			MediaExit();
+			return (MediaInit());
+		}
+		if (!nVidFullscreen) {
+//			ScrnSize(); // dont' change any screens on the wii
+		}
+
+		if (!bVidOkay && (bDrvOkay || bVidUsePlaceholder)) {
+			// Make sure the error will be visible
+//			SplashDestroy(1);
+
+	//		FBAPopupAddText(PUF_TEXT_DEFAULT, MAKEINTRESOURCE(IDS_ERR_UI_MODULE), VidGetModuleName());
+	//		FBAPopupDisplay(PUF_TYPE_ERROR);
+		}
+
+		if (bVidOkay && ((bRunPause && bAltPause) || !bDrvOkay)) {
+			VidRedraw();
+		}
+	}
+
+	return 0;
+}
+
+int MediaExit()
+{
+	nBurnSoundRate = 0;					// Blank sound
+	pBurnSoundOut = NULL;
+
+	AudSoundExit();						// Exit Dsound
+
+	VidExit();
+
+	InputExit();
+
+//	DestroyWindow(hInpsDlg);			// Make sure the Input Set dialog is exitted
+//	DestroyWindow(hInpdDlg);			// Make sure the Input Dialog is exitted
+
+//	ScrnExit();							// Exit the Scrn Window
+
+	return 0;
+}
